cd78d53da04e08efa2c7b79b093684dacb8204eb
OmidSargazi <om.sargazi@gmail.com>
2023-07-18 00:53:44 +0330

    install react-router-dom and create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { green } from "@material-ui/core/colors";
+import { ChannelId, Feed, Navbar, SearchFeed, VideoDetail } from "./components";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelId />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
